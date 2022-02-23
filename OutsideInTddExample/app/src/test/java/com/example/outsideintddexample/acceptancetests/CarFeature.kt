dbb0310629d5978a0b3996d80387482deb4befa9
ch <mkmsacc@outlook.com>
2022-02-23 07:41:49 +0700

    Add Failing test

--- OutsideInTddExample/app/src/test/java/com/example/outsideintddexample/acceptancetests/CarFeature.kt
+++ OutsideInTddExample/app/src/test/java/com/example/outsideintddexample/acceptancetests/CarFeature.kt
@@ -2,16 +2,24 @@ package com.example.outsideintddexample.acceptancetests
 
 import com.example.outsideintddexample.Car
 import junit.framework.TestCase
+import junit.framework.TestCase.*
 import org.junit.Test
 
 class CarFeature {
-
-    private val car = Car(6.0)
+    private val engine = Engine()
+    private val car = Car(6.0, engine)
 
     @Test
     fun carIsLoosingFuelWhenItTurnsOn() {
         car.turnOn()
-        TestCase.assertEquals(5.5, car.fuel)
+        assertEquals(5.5, car.fuel)
 
+    }
+
+    @Test
+    fun carIsTurningOnItsEngineAndIncreasesTheTemperature() {
+        car.turnOn()
+        assertTrue( car.engine.isTurnedOn)
+        assertEquals(95, car.engine.temperature)
     }
 }
