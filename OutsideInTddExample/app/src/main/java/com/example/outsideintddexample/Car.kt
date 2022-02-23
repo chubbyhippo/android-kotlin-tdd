31c2e86d1829a1d114ecd84b964f7a460b4573d7
ch <mkmsacc@outlook.com>
2022-02-23 08:23:59 +0700

    Add engine to car

--- OutsideInTddExample/app/src/main/java/com/example/outsideintddexample/Car.kt
+++ OutsideInTddExample/app/src/main/java/com/example/outsideintddexample/Car.kt
@@ -1,8 +1,12 @@
 package com.example.outsideintddexample
 
-class Car(var fuel: Double) {
+class Car(
+    var fuel: Double,
+    val engine: Engine
+) {
     fun turnOn() {
         fuel -= 0.5
+        engine.turnOn()
     }
 
 }
