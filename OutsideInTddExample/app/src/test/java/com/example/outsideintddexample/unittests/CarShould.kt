21ff8cc8c5056f52fedd7cd0bcc2bfc1bebb0c6f
ch <mkmsacc@outlook.com>
2022-02-23 08:24:45 +0700

    Add test

--- OutsideInTddExample/app/src/test/java/com/example/outsideintddexample/unittests/CarShould.kt
+++ OutsideInTddExample/app/src/test/java/com/example/outsideintddexample/unittests/CarShould.kt
@@ -1,16 +1,28 @@
 package com.example.outsideintddexample.unittests
 
 import com.example.outsideintddexample.Car
+import com.example.outsideintddexample.Engine
 import junit.framework.TestCase.assertEquals
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.times
+import org.mockito.kotlin.verify
 
 class CarShould {
-    private val car = Car(5.0)
+
+     private val engine = mock<Engine>()
+    private val car = Car(5.0, engine)
 
     @Test
     fun looseFuelWhenItTurnsOn() {
         car.turnOn()
 
         assertEquals(4.5, car.fuel)
+    }
+
+    @Test
+    fun turnOnItsEngine() {
+        car.turnOn()
+        verify(engine, times(1)).turnOn()
     }
 }
