f1d794d532f7511868975ad5c2377f7eeea2c23e
ch <mkmsacc@outlook.com>
2022-02-24 07:52:10 +0700

    Uncomment CarFeature

--- OutsideInTddExample/app/src/test/java/com/example/outsideintddexample/acceptancetests/CarFeature.kt
+++ OutsideInTddExample/app/src/test/java/com/example/outsideintddexample/acceptancetests/CarFeature.kt
@@ -6,20 +6,20 @@ import junit.framework.TestCase.*
 import org.junit.Test
 
 class CarFeature {
-//    private val engine = Engine()
-//    private val car = Car(6.0, engine)
-//
-//    @Test
-//    fun carIsLoosingFuelWhenItTurnsOn() {
-//        car.turnOn()
-//        assertEquals(5.5, car.fuel)
-//
-//    }
-//
-//    @Test
-//    fun carIsTurningOnItsEngineAndIncreasesTheTemperature() {
-//        car.turnOn()
-//        assertTrue( car.engine.isTurnedOn)
+    private val engine = Engine()
+    private val car = Car(6.0, engine)
+
+    @Test
+    fun carIsLoosingFuelWhenItTurnsOn() {
+        car.turnOn()
+        assertEquals(5.5, car.fuel)
+
+    }
+
+    @Test
+    fun carIsTurningOnItsEngineAndIncreasesTheTemperature() {
+        car.turnOn()
+        assertTrue( car.engine.isTurnedOn)
 //        assertEquals(95, car.engine.temperature)
-//    }
+    }
 }
