f696353d389d76fc36df4b52e0346ca0a5401496
ch <mkmsacc@outlook.com>
2022-02-25 07:29:22 +0700

    Make the test pass

--- OutsideInTddExample/app/src/test/java/com/example/outsideintddexample/unittests/EngineShould.kt
+++ OutsideInTddExample/app/src/test/java/com/example/outsideintddexample/unittests/EngineShould.kt
@@ -1,6 +1,7 @@
 package com.example.outsideintddexample.unittests
 
 import com.example.outsideintddexample.Engine
+import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
 import org.junit.Test
 
@@ -12,5 +13,11 @@ class EngineShould {
     fun turnOn() {
         engine.turnOn()
         assertTrue(engine.isTurnedOn)
+    }
+
+    @Test
+    fun riseTheTemperatureWhenItTurnsOn() {
+        engine.turnOn()
+        assertEquals(95, engine.temperature)
     }
 }
